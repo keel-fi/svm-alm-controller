19a1742da759384fdbe7046a1e073eab67f3a302
iurage <iurage@protonmail.com>
2025-05-23 03:57:26 +0800

    Refactor

--- program/src/processor/mod.rs
+++ program/src/processor/mod.rs
@@ -1,28 +1,24 @@
 pub mod emit_event;
-pub mod initalize_oracle;
 pub mod initialize_controller;
 pub mod initialize_integration;
 pub mod initialize_reserve;
 pub mod manage_integration;
 pub mod manage_permission;
 pub mod manage_reserve;
+pub mod oracle;
 pub mod pull;
 pub mod push;
-pub mod refresh_oracle;
 pub mod sync_integration;
 pub mod sync_reserve;
-pub mod update_oracle;
 
 pub use emit_event::*;
-pub use initalize_oracle::*;
 pub use initialize_controller::*;
 pub use initialize_integration::*;
 pub use initialize_reserve::*;
 pub use manage_integration::*;
 pub use manage_permission::*;
 pub use manage_reserve::*;
-pub use refresh_oracle::*;
-pub use update_oracle::*;
+pub use oracle::*;
 
 pub use pull::*;
 pub use push::*;
