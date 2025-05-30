2439408d75ac1633aa2ddb87451aea39f8380726
iurage <iurage@protonmail.com>
2025-05-30 20:34:39 +0800

    Refactor

--- integration_tests/tests/swap/test_atomic_swap.rs
+++ integration_tests/tests/swap/test_atomic_swap.rs
@@ -11,8 +11,8 @@ mod tests {
     use solana_sdk::{clock::Clock, pubkey::Pubkey, signature::Keypair, signer::Signer};
     use spl_associated_token_account_client::address::get_associated_token_address_with_program_id;
     use svm_alm_controller_client::generated::types::{
-        AtomicSwapConfig, ControllerStatus, InitializeArgs, IntegrationConfig, IntegrationStatus,
-        PermissionStatus, ReserveStatus,
+        AtomicSwapConfig, ControllerStatus, InitializeArgs, IntegrationConfig, IntegrationState,
+        IntegrationStatus, PermissionStatus, ReserveStatus,
     };
 
     use crate::{
@@ -37,36 +37,44 @@ mod tests {
         svm
     }
 
+    struct SwapEnv {
+        pub relayer_authority_kp: Keypair,
+        pub mint_authority: Keypair,
+        pub price_feed: Pubkey,
+        pub nonce: Pubkey,
+        pub oracle: Pubkey,
+        pub pc_token_mint: Pubkey,
+        pub coin_token_mint: Pubkey,
+        pub controller_pk: Pubkey,
+        pub atomic_swap_integration_pk: Pubkey,
+        pub relayer_pc: Pubkey,
+        pub relayer_coin: Pubkey,
+        pub pc_reserve_pubkey: Pubkey,
+        pub pc_reserve_vault: Pubkey,
+        pub coin_reserve_pubkey: Pubkey,
+        pub coin_reserve_vault: Pubkey,
+        pub permission_pda: Pubkey,
+    }
+
     fn setup_integration_env(
         svm: &mut LiteSVM,
-        relayer_authority_kp: &Keypair,
-        price_feed: &Pubkey,
-        nonce: &Pubkey,
-        coin_token_mint: &Pubkey,
-        pc_token_mint: &Pubkey,
-        mint_authority: &Keypair,
         expiry_timestamp: i64,
-    ) -> Result<
-        (
-            Pubkey,
-            Pubkey,
-            Pubkey,
-            Pubkey,
-            Pubkey,
-            Pubkey,
-            Pubkey,
-            Pubkey,
-        ),
-        Box<dyn std::error::Error>,
-    > {
+    ) -> Result<SwapEnv, Box<dyn std::error::Error>> {
+        let relayer_authority_kp = Keypair::new();
+        let price_feed = Pubkey::new_unique();
+        let nonce = Pubkey::new_unique();
+        let coin_token_mint = Pubkey::new_unique();
+        let pc_token_mint = Pubkey::new_unique();
+        let mint_authority = Keypair::new();
+
         svm.airdrop(&relayer_authority_kp.pubkey(), 100_000_000)
             .unwrap();
 
         // Initialize price feed and oracle.
         let update_slot = 1000_000;
         svm.warp_to_slot(update_slot);
-        set_price_feed(svm, price_feed, 1_000_000_000)?;
-        initalize_oracle(svm, &relayer_authority_kp, nonce, price_feed, 0, false)?;
+        set_price_feed(svm, &price_feed, 1_000_000_000)?;
+        initalize_oracle(svm, &relayer_authority_kp, &nonce, &price_feed, 0, false)?;
 
         setup_token_mint(svm, &coin_token_mint, 6, &mint_authority.pubkey());
         setup_token_mint(svm, &pc_token_mint, 6, &mint_authority.pubkey());
@@ -107,8 +115,8 @@ mod tests {
             1_000_000_000_000, // rate_limit_slope
             1_000_000_000_000, // rate_limit_max_outflow
             &IntegrationConfig::AtomicSwap(AtomicSwapConfig {
-                input_token: *pc_token_mint,
-                output_token: *coin_token_mint,
+                input_token: pc_token_mint,
+                output_token: coin_token_mint,
                 oracle,
                 max_slippage_bps: 123,
                 max_staleness: 100,
@@ -200,7 +208,16 @@ mod tests {
             600_000_000,
         )?;
 
-        Ok((
+        let permission_pda = derive_permission_pda(&controller_pk, &relayer_authority_kp.pubkey());
+
+        Ok(SwapEnv {
+            relayer_authority_kp,
+            mint_authority,
+            price_feed,
+            nonce,
+            oracle,
+            pc_token_mint,
+            coin_token_mint,
             controller_pk,
             atomic_swap_integration_pk,
             relayer_pc,
@@ -209,42 +226,39 @@ mod tests {
             pc_reserve_vault,
             coin_reserve_pubkey,
             coin_reserve_vault,
-        ))
+            permission_pda,
+        })
     }
 
     #[test_log::test]
     fn init_atomic_swap() -> Result<(), Box<dyn std::error::Error>> {
         let mut svm = lite_svm_with_programs();
 
-        let relayer_authority_kp = Keypair::new();
-        let price_feed = Pubkey::new_unique();
-        let nonce = Pubkey::new_unique();
-        let coin_token_mint = Pubkey::new_unique();
-        let pc_token_mint = Pubkey::new_unique();
-        let mint_authority = Keypair::new();
         let expiry_timestamp = svm.get_sysvar::<Clock>().unix_timestamp + 1000;
+        let swap_env = setup_integration_env(&mut svm, expiry_timestamp)?;
 
-        let (
-            controller_pk,
-            atomic_swap_integration_pk,
-            _relayer_pc,
-            _relayer_coin,
-            _pc_reserve_pubkey,
-            _pc_reserve_vault,
-            _coin_reserve_pubkey,
-            _coin_reserve_vault,
-        ) = setup_integration_env(
-            &mut svm,
-            &relayer_authority_kp,
-            &price_feed,
-            &nonce,
-            &coin_token_mint,
-            &pc_token_mint,
-            &mint_authority,
-            expiry_timestamp,
-        )?;
-        let integration = fetch_integration_account(&mut svm, &atomic_swap_integration_pk)?;
-        assert!(integration.is_some(), "Integration account is not found");
+        // Check that integration after init.
+        let integration =
+            fetch_integration_account(&mut svm, &swap_env.atomic_swap_integration_pk)?.unwrap();
+
+        if let (IntegrationConfig::AtomicSwap(cfg), IntegrationState::AtomicSwap(state)) =
+            (&integration.config, integration.state)
+        {
+            assert_eq!(cfg.input_token, swap_env.pc_token_mint);
+            assert_eq!(cfg.output_token, swap_env.coin_token_mint);
+            assert_eq!(cfg.oracle, swap_env.oracle);
+            assert_eq!(cfg.max_slippage_bps, 123);
+            assert_eq!(cfg.max_staleness, 100);
+            assert_eq!(cfg.input_mint_decimals, 6);
+            assert_eq!(cfg.output_mint_decimals, 6);
+            assert_eq!(cfg.expiry_timestamp, expiry_timestamp);
+
+            assert_eq!(state.last_balance_a, 0);
+            assert_eq!(state.last_balance_b, 0);
+            assert_eq!(state.amount_borrowed, 0);
+        } else {
+            assert!(false)
+        }
 
         Ok(())
     }
@@ -252,44 +266,17 @@ mod tests {
     #[test_log::test]
     fn atomic_swap_success() -> Result<(), Box<dyn std::error::Error>> {
         let mut svm = lite_svm_with_programs();
-        let relayer_authority_kp = Keypair::new();
-        let price_feed = Pubkey::new_unique();
-        let nonce = Pubkey::new_unique();
-        let coin_token_mint = Pubkey::new_unique();
-        let pc_token_mint = Pubkey::new_unique();
-        let mint_authority = Keypair::new();
-        let oracle = derive_oracle_pda(&nonce);
+
         let expiry_timestamp = svm.get_sysvar::<Clock>().unix_timestamp + 1000;
+        let swap_env = setup_integration_env(&mut svm, expiry_timestamp)?;
 
-        let (
-            controller_pk,
-            atomic_swap_integration_pk,
-            relayer_pc,
-            relayer_coin,
-            _pc_reserve_pubkey,
-            pc_reserve_vault,
-            _coin_reserve_pubkey,
-            coin_reserve_vault,
-        ) = setup_integration_env(
-            &mut svm,
-            &relayer_authority_kp,
-            &price_feed,
-            &nonce,
-            &coin_token_mint,
-            &pc_token_mint,
-            &mint_authority,
-            expiry_timestamp,
-        )?;
-        let integration = fetch_integration_account(&mut svm, &atomic_swap_integration_pk)?;
-        assert!(integration.is_some(), "Integration account is not found");
+        let integration =
+            fetch_integration_account(&mut svm, &swap_env.atomic_swap_integration_pk)?;
 
-        let vault_a_before = fetch_token_account(&mut svm, &pc_reserve_vault);
-        let vault_b_before = fetch_token_account(&mut svm, &coin_reserve_vault);
-        let relayer_a_before = fetch_token_account(&mut svm, &relayer_pc);
-        let relayer_b_before = fetch_token_account(&mut svm, &relayer_coin);
-
-        let calling_permission_pda =
-            derive_permission_pda(&controller_pk, &relayer_authority_kp.pubkey());
+        let vault_a_before = fetch_token_account(&mut svm, &swap_env.pc_reserve_vault);
+        let vault_b_before = fetch_token_account(&mut svm, &swap_env.coin_reserve_vault);
+        let relayer_a_before = fetch_token_account(&mut svm, &swap_env.relayer_pc);
+        let relayer_b_before = fetch_token_account(&mut svm, &swap_env.relayer_coin);
 
         let borrow_amount = 100;
         let repay_amount_a = 0;
@@ -297,16 +284,16 @@ mod tests {
 
         atomic_swap_borrow_repay(
             &mut svm,
-            &relayer_authority_kp,
-            controller_pk,
-            calling_permission_pda,
-            atomic_swap_integration_pk,
-            pc_token_mint,
-            coin_token_mint,
-            oracle,
-            price_feed,
-            relayer_pc,   // payer_account_a
-            relayer_coin, // payer_account_b
+            &swap_env.relayer_authority_kp,
+            swap_env.controller_pk,
+            swap_env.permission_pda,
+            swap_env.atomic_swap_integration_pk,
+            swap_env.pc_token_mint,
+            swap_env.coin_token_mint,
+            swap_env.oracle,
+            swap_env.price_feed,
+            swap_env.relayer_pc,   // payer_account_a
+            swap_env.relayer_coin, // payer_account_b
             pinocchio_token::ID.into(),
             pinocchio_token::ID.into(),
             borrow_amount,
@@ -314,10 +301,10 @@ mod tests {
             repay_amount_b,
         )?;
 
-        let vault_a_after = fetch_token_account(&mut svm, &pc_reserve_vault);
-        let vault_b_after = fetch_token_account(&mut svm, &coin_reserve_vault);
-        let relayer_a_after = fetch_token_account(&mut svm, &relayer_pc);
-        let relayer_b_after = fetch_token_account(&mut svm, &relayer_coin);
+        let vault_a_after = fetch_token_account(&mut svm, &swap_env.pc_reserve_vault);
+        let vault_b_after = fetch_token_account(&mut svm, &swap_env.coin_reserve_vault);
+        let relayer_a_after = fetch_token_account(&mut svm, &swap_env.relayer_pc);
+        let relayer_b_after = fetch_token_account(&mut svm, &swap_env.relayer_coin);
 
         let vault_a_decrease = vault_a_before
             .amount
@@ -342,8 +329,28 @@ mod tests {
         assert_eq!(vault_b_increase, repay_amount_b);
         assert_eq!(relayer_b_decrease, repay_amount_b);
 
-        // TODO: Check that state is reset.
-        let integration = fetch_integration_account(&mut svm, &atomic_swap_integration_pk)?;
+        // Check that integration after swap.
+        let integration =
+            fetch_integration_account(&mut svm, &swap_env.atomic_swap_integration_pk)?.unwrap();
+
+        if let (IntegrationConfig::AtomicSwap(cfg), IntegrationState::AtomicSwap(state)) =
+            (&integration.config, integration.state)
+        {
+            assert_eq!(cfg.input_token, swap_env.pc_token_mint);
+            assert_eq!(cfg.output_token, swap_env.coin_token_mint);
+            assert_eq!(cfg.oracle, swap_env.oracle);
+            assert_eq!(cfg.max_slippage_bps, 123);
+            assert_eq!(cfg.max_staleness, 100);
+            assert_eq!(cfg.input_mint_decimals, 6);
+            assert_eq!(cfg.output_mint_decimals, 6);
+            assert_eq!(cfg.expiry_timestamp, expiry_timestamp);
+
+            assert_eq!(state.last_balance_a, 0);
+            assert_eq!(state.last_balance_b, 0);
+            assert_eq!(state.amount_borrowed, 0);
+        } else {
+            assert!(false)
+        }
 
         Ok(())
     }
