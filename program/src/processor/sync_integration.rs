c3bf8e83a9a44a6a422347a4d7346852096ce9e5
sebatustra <sebatustra@protonmail.com>
2025-08-20 11:30:46 -0400

    Removed Suspended reserve check for sync instruction.

--- program/src/processor/sync_integration.rs
+++ program/src/processor/sync_integration.rs
@@ -1,5 +1,5 @@
 use crate::{
-    define_account_struct, enums::{IntegrationConfig, ReserveStatus}, error::SvmAlmControllerErrors, integrations::{spl_token_swap::sync::process_sync_spl_token_swap, utilization_market::{config::UtilizationMarketConfig, kamino::sync::process_sync_kamino}}, state::{nova_account::NovaAccount, Controller, Integration, Reserve}
+    define_account_struct, enums::IntegrationConfig, integrations::{spl_token_swap::sync::process_sync_spl_token_swap, utilization_market::{config::UtilizationMarketConfig, kamino::sync::process_sync_kamino}}, state::{nova_account::NovaAccount, Controller, Integration, Reserve}
 };
 use pinocchio::{
     account_info::AccountInfo,
@@ -44,9 +44,6 @@ pub fn process_sync_integration(
 
     // Load in the reserve account (kamino only handles one reserve)
     let mut reserve = Reserve::load_and_check_mut(ctx.reserve, ctx.controller.key())?;
-    if reserve.status != ReserveStatus::Active {
-        return Err(SvmAlmControllerErrors::ReserveStatusDoesNotPermitAction.into());
-    }
 
     // Depending on the integration, there may be an
     //  inner (integration-specific) sync logic to call
