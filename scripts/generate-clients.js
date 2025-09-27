469eaa4b78df9b0348ac176474e8972e656f9870
Taylor123 <nftaylor11@gmail.com>
2025-09-27 16:10:45 -0400

    update codama and init TS with manual fixes for unnecessary FeedArgs

--- scripts/generate-clients.js
+++ scripts/generate-clients.js
@@ -1,18 +1,27 @@
 const codama = require("codama");
 const anchorIdl = require("@codama/nodes-from-anchor");
 const path = require("path");
-const renderers = require("@codama/renderers");
+const jsRenderers = require("@codama/renderers-js");
+const rustRenderers = require("@codama/renderers-rust");
 
 const projectRoot = path.join(__dirname, "..");
 const idlDir = path.join(projectRoot, "idl");
 const idl = require(path.join(idlDir, "svm_alm_controller.json"));
 const rustClientsDir = path.join(__dirname, "..", "clients", "rust");
+const tsClient = path.join(__dirname, "..", "clients", "ts");
 
 const Codama = codama.createFromRoot(anchorIdl.rootNodeFromAnchor(idl));
+// Rust Client
+// Codama.accept(
+//   rustRenderers.renderVisitor(path.join(rustClientsDir, "src", "generated"), {
+//     formatCode: true,
+//     crateFolder: rustClientsDir,
+//     deleteFolderBeforeRendering: true,
+//   })
+// );
+// TS Client
 Codama.accept(
-  renderers.renderRustVisitor(path.join(rustClientsDir, "src", "generated"), {
-    formatCode: true,
-    crateFolder: rustClientsDir,
-    deleteFolderBeforeRendering: true,
+  jsRenderers.renderVisitor(path.join(tsClient, "src", "generated"), {
+    nameTransformers: {},
   })
 );
