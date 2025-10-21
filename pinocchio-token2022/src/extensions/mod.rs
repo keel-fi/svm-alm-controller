a501b60ce4bcaa5583c3d6529f49b77687a71e30
Taylor Johnson <nftaylor11@gmail.com>
2025-10-21 18:48:01 -0400

    Trim pt22 (#95)
    * remove unused instructions/code for token extensions
    
    * replace instructions and state with canonical pinocchio-token-2022 crate

--- pinocchio-token2022/src/extensions/mod.rs
+++ pinocchio-token2022/src/extensions/mod.rs
@@ -2,63 +2,10 @@ use crate::{
     from_bytes_ref,
     state::{Mint, TokenAccount},
 };
-// pub mod confidential_transfer;
-pub mod confidential_mint_burn;
-pub mod confidential_transfer;
-pub mod confidential_transfer_fee;
-pub mod cpi_guard;
-pub mod default_account_state;
-pub mod group_member_pointer;
-pub mod group_pointer;
-pub mod immutable_owner;
-pub mod interest_bearing_mint;
-pub mod memo_transfer;
-pub mod metadata;
-pub mod metadata_pointer;
-pub mod mint_close_authority;
-pub mod non_transferable;
+
 pub mod pausable;
-pub mod permanent_delegate;
-pub mod reallocate;
-pub mod scaled_ui_amount;
-pub mod token_group;
 pub mod transfer_fee;
 pub mod transfer_hook;
-
-pub const ELGAMAL_PUBKEY_LEN: usize = 32;
-
-pub const POD_AE_CIPHERTEXT_LEN: usize = 36;
-pub const POD_ELGAMAL_CIPHERTEXT_LEN: usize = 64;
-
-/// Local definition mirroring spl_token_confidential_transfer::pod::PodElGamalCiphertext
-#[derive(Clone, Copy, Debug, PartialEq)]
-#[repr(C)]
-pub struct PodElGamalCiphertext(pub [u8; POD_ELGAMAL_CIPHERTEXT_LEN]);
-
-impl Default for PodElGamalCiphertext {
-    fn default() -> Self {
-        Self([0u8; POD_ELGAMAL_CIPHERTEXT_LEN])
-    }
-}
-
-/// Local definition mirroring spl_token_confidential_transfer::pod::PodAeCiphertext
-#[derive(Clone, Copy, Debug, PartialEq)]
-#[repr(C)]
-pub struct PodAeCiphertext(pub [u8; POD_AE_CIPHERTEXT_LEN]);
-
-impl Default for PodAeCiphertext {
-    fn default() -> Self {
-        Self([0u8; POD_AE_CIPHERTEXT_LEN])
-    }
-}
-
-/// Alias for clarity, mirroring spl_token_confidential_transfer::instruction::DecryptableBalance
-pub type DecryptableBalance = PodAeCiphertext;
-/// Alias for clarity, mirroring spl_token_confidential_transfer::state::EncryptedBalance
-pub type EncryptedBalance = PodElGamalCiphertext;
-
-#[derive(Debug, Clone, Copy, Default, PartialEq)]
-pub struct PodElGamalPubkey(pub [u8; ELGAMAL_PUBKEY_LEN]);
 
 pub const EXTENSIONS_PADDING: usize = 83;
 
@@ -241,59 +188,9 @@ pub fn get_extension_from_bytes<T: Extension + Clone + Copy>(acc_data_bytes: &[u
     None
 }
 
-pub fn get_extension_data_bytes_for_variable_pack<T: Extension + Clone>(
-    acc_data_bytes: &[u8],
-) -> Option<&[u8]> {
-    let ext_bytes = match T::BASE_STATE {
-        BaseState::Mint => {
-            &acc_data_bytes[Mint::BASE_LEN + EXTENSIONS_PADDING + EXTENSION_START_OFFSET..]
-        }
-        BaseState::TokenAccount => {
-            &acc_data_bytes[TokenAccount::BASE_LEN + EXTENSION_START_OFFSET..]
-        }
-        _ => return None,
-    };
-    let mut start = 0;
-    let end = ext_bytes.len();
-    while start < end {
-        let ext_type_idx = start;
-        let ext_len_idx = ext_type_idx + 2;
-        let ext_data_idx = ext_len_idx + EXTENSION_LENGTH_LEN;
-
-        let ext_type: [u8; 2] = ext_bytes[ext_type_idx..ext_type_idx + EXTENSION_TYPE_LEN]
-            .try_into()
-            .ok()?;
-
-        let ext_type = ExtensionType::from_bytes(ext_type)?;
-        let ext_len: [u8; 2] = ext_bytes[ext_len_idx..ext_len_idx + EXTENSION_LENGTH_LEN]
-            .try_into()
-            .ok()?;
-
-        let ext_len = u16::from_le_bytes(ext_len);
-
-        if ext_type == T::TYPE {
-            return Some(&ext_bytes[ext_data_idx..ext_data_idx + ext_len as usize]);
-        }
-
-        start = start + EXTENSION_TYPE_LEN + EXTENSION_LENGTH_LEN + ext_len as usize;
-    }
-    None
-}
-
 #[cfg(test)]
 mod tests {
-    use crate::extensions::{
-        confidential_transfer::{ConfidentialTransferFeeConfig, ConfidentialTransferMint},
-        get_extension_from_bytes,
-        group_member_pointer::GroupMemberPointer,
-        group_pointer::GroupPointer,
-        metadata::TokenMetadata,
-        metadata_pointer::MetadataPointer,
-        mint_close_authority::MintCloseAuthority,
-        permanent_delegate::PermanentDelegate,
-        token_group::TokenGroup,
-        transfer_fee::TransferFeeConfig,
-    };
+    use crate::extensions::{get_extension_from_bytes, transfer_fee::TransferFeeConfig};
 
     pub const TEST_MINT_WITH_EXTENSIONS_SLICE: &[u8] = &[
         1, 0, 0, 0, 221, 76, 72, 108, 144, 248, 182, 240, 7, 195, 4, 239, 36, 129, 248, 5, 24, 107,
@@ -354,109 +251,11 @@ mod tests {
         1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
         2, 2, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0,
     ];
-    pub const TEST_GROUP_MEMBER_MINT_SLICE: &[u8] = &[
-        1, 0, 0, 0, 221, 76, 72, 108, 144, 248, 182, 240, 7, 195, 4, 239, 36, 129, 248, 5, 24, 107,
-        232, 253, 95, 82, 172, 209, 2, 92, 183, 155, 159, 103, 255, 33, 133, 204, 6, 44, 35, 140,
-        0, 0, 6, 1, 1, 0, 0, 0, 23, 133, 50, 97, 239, 106, 184, 83, 42, 103, 240, 83, 134, 90, 173,
-        49, 41, 63, 207, 7, 207, 18, 10, 181, 185, 161, 87, 6, 84, 141, 192, 43, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-        /*                  GroupMemberPointer Extension                                      */
-        22, 0, 64, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-        2, 2, 2, 2, 2, 2, 2, 2,
-    ];
 
     #[test]
     fn test_transfer_fee_config() {
         let transfer_fee =
             get_extension_from_bytes::<TransferFeeConfig>(&TEST_MINT_WITH_EXTENSIONS_SLICE);
         assert!(transfer_fee.is_some());
-    }
-
-    #[test]
-    fn test_metadata_pointer() {
-        let metadata_pointer =
-            get_extension_from_bytes::<MetadataPointer>(&TEST_MINT_WITH_EXTENSIONS_SLICE);
-        assert!(metadata_pointer.is_some());
-    }
-
-    #[test]
-    fn test_mint_close_authority() {
-        let mint_close_authority =
-            get_extension_from_bytes::<MintCloseAuthority>(&TEST_MINT_WITH_EXTENSIONS_SLICE);
-        assert!(mint_close_authority.is_some());
-    }
-
-    #[test]
-    fn test_permanent_delegate() {
-        let permanent_delegate =
-            get_extension_from_bytes::<PermanentDelegate>(&TEST_MINT_WITH_EXTENSIONS_SLICE);
-        assert!(permanent_delegate.is_some());
-    }
-
-    #[test]
-    fn test_group_pointer() {
-        let group_pointer =
-            get_extension_from_bytes::<GroupPointer>(&TEST_MINT_WITH_EXTENSIONS_SLICE);
-        assert!(group_pointer.is_some());
-        let gp = group_pointer.unwrap();
-        assert!(gp.authority.eq(&[1u8; 32]));
-        assert!(gp.group_address.eq(&[2u8; 32]));
-    }
-
-    #[test]
-    fn test_token_group() {
-        let token_group = get_extension_from_bytes::<TokenGroup>(&TEST_MINT_WITH_EXTENSIONS_SLICE);
-        assert!(token_group.is_some());
-        let tg = token_group.unwrap();
-        assert!(tg.update_authority.eq(&[1u8; 32]));
-        assert!(tg.mint.eq(&[2u8; 32]));
-        assert_eq!(u64::from_le_bytes(tg.size), 1);
-        assert_eq!(u64::from_le_bytes(tg.max_size), 2);
-    }
-
-    #[test]
-    fn test_group_member_pointer() {
-        let group_member_pointer =
-            get_extension_from_bytes::<GroupMemberPointer>(&TEST_GROUP_MEMBER_MINT_SLICE);
-        assert!(group_member_pointer.is_some());
-        let gmp = group_member_pointer.unwrap();
-        assert!(gmp.authority.eq(&[1u8; 32]));
-        assert!(gmp.member_address.eq(&[2u8; 32]));
-    }
-
-    #[test]
-    fn test_confidential_transfer_mint() {
-        let confidential_transfer_mint =
-            get_extension_from_bytes::<ConfidentialTransferMint>(&TEST_MINT_WITH_EXTENSIONS_SLICE);
-        assert!(confidential_transfer_mint.is_some());
-    }
-
-    #[test]
-    fn test_confidential_transfer_fee_config() {
-        let confidential_transfer_fee_config = get_extension_from_bytes::<
-            ConfidentialTransferFeeConfig,
-        >(&TEST_MINT_WITH_EXTENSIONS_SLICE);
-        assert!(confidential_transfer_fee_config.is_some());
-    }
-
-    #[test]
-    fn test_token_metadata() {
-        use crate::extensions::get_extension_data_bytes_for_variable_pack;
-
-        let token_metadata = get_extension_data_bytes_for_variable_pack::<TokenMetadata>(
-            &TEST_MINT_WITH_EXTENSIONS_SLICE,
-        )
-        .unwrap();
-
-        let token_metadata = TokenMetadata::from_bytes(token_metadata);
-
-        assert!(token_metadata.is_ok());
-
-        let token_metadata = token_metadata.unwrap();
-
-        assert_eq!(token_metadata.symbol, "PYUSD");
     }
 }
--- pinocchio-token2022/src/lib.rs
+++ pinocchio-token2022/src/lib.rs
@@ -1,14 +1,8 @@
 #![no_std]
 
 pub mod extensions;
-pub mod instructions;
-pub mod state;
 
-pinocchio_pubkey::declare_id!("TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb");
-
-use core::mem::MaybeUninit;
-
-const UNINIT_BYTE: MaybeUninit<u8> = MaybeUninit::<u8>::uninit();
+pub use pinocchio_token_2022::*;
 
 /// Deserialize a type from a byte array.
 ///
@@ -28,11 +22,4 @@ pub unsafe fn from_bytes<T: Clone + Copy>(data: &[u8]) -> T {
 pub unsafe fn from_bytes_ref<T: Clone + Copy>(data: &[u8]) -> &T {
     assert_eq!(data.len(), core::mem::size_of::<T>());
     &*(data.as_ptr() as *const T)
-}
-
-#[inline(always)]
-fn write_bytes(destination: &mut [MaybeUninit<u8>], source: &[u8]) {
-    for (d, s) in destination.iter_mut().zip(source.iter()) {
-        d.write(*s);
-    }
 }
