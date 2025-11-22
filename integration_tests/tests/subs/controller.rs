3dc33155d6163db73a351e3853315e0de8e1f8e3
Seba <124921995+sebatustra@users.noreply.github.com>
2025-11-22 12:38:42 -0300

    add missing permission event in init controller ix (#140)

--- integration_tests/tests/subs/controller.rs
+++ integration_tests/tests/subs/controller.rs
@@ -10,7 +10,7 @@ use svm_alm_controller_client::generated::{
     accounts::Controller,
     instructions::{InitializeControllerBuilder, ManageControllerBuilder},
     programs::SVM_ALM_CONTROLLER_ID,
-    types::{ControllerStatus, ControllerUpdateEvent, PermissionStatus, SvmAlmControllerEvent},
+    types::{ControllerStatus, ControllerUpdateEvent, PermissionStatus, PermissionUpdateEvent, SvmAlmControllerEvent},
 };
 
 use crate::{
@@ -141,17 +141,17 @@ pub fn initialize_contoller(
 
     let controller = controller.unwrap();
 
-    // assert expected event
-    let expected_event = SvmAlmControllerEvent::ControllerUpdate(ControllerUpdateEvent {
+    // assert expected controller event
+    let expected_controller_event = SvmAlmControllerEvent::ControllerUpdate(ControllerUpdateEvent {
         controller: controller_pda,
         authority: authority.pubkey(),
         old_state: None,
         new_state: Some(controller.clone()),
     });
     assert_contains_controller_cpi_event!(
-        tx_result.unwrap(),
+        tx_result.clone().unwrap(),
         txn.message.account_keys.as_slice(),
-        expected_event
+        expected_controller_event
     );
 
     assert_eq!(
@@ -165,6 +165,22 @@ pub fn initialize_contoller(
 
     assert!(permission.is_some(), "Permission account is not found");
     let permission = permission.unwrap();
+
+    // assert expected permission event
+    let expected_permission_event = SvmAlmControllerEvent::PermissionUpdate(PermissionUpdateEvent {
+        controller: controller_pda,
+        permission: permission_pda,
+        authority: authority.pubkey(),
+        old_state: None,
+        new_state: Some(permission.clone()),
+    });
+    assert_contains_controller_cpi_event!(
+        tx_result.unwrap(),
+        txn.message.account_keys.as_slice(),
+        expected_permission_event
+    );
+
+
     assert_eq!(
         permission.authority,
         authority.pubkey(),
