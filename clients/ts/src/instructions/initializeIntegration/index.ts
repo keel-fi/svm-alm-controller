c852f2f24a7dee4174d8b4b8e7207e0013a983df
Matt Auer <mattauer@umich.edu>
2025-12-29 10:05:29 -0600

    add integrations

--- clients/ts/src/instructions/initializeIntegration/index.ts
+++ clients/ts/src/instructions/initializeIntegration/index.ts
@@ -1,8 +1,8 @@
 export * from "./atomicSwap";
+export * from "./drift";
+export * from "./kaminoLend";
 // TODO: Add other integration types:
 // export * from "./cctpBridge";
-// export * from "./drift";
-// export * from "./kaminoLend";
 // export * from "./lzBridge";
 // export * from "./splTokenExternal";
 
